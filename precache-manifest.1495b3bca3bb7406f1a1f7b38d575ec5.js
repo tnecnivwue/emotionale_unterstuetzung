09a670a58b0f815a3abf0fb8161f65945c146f91
tnecnivwue <vincent.wuestefeld@web.de>
2024-01-17 17:54:19 +0100

    host

---
+++ precache-manifest.1495b3bca3bb7406f1a1f7b38d575ec5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3ad354e5d16a7e2eb3e515f2987205",
+    "url": "./emotionale_unterstuetzung/index.html"
+  },
+  {
+    "revision": "6b7320b70aa50f0c523b",
+    "url": "./emotionale_unterstuetzung/static/css/2.016185da.chunk.css"
+  },
+  {
+    "revision": "0a41759d87c207fe0e93",
+    "url": "./emotionale_unterstuetzung/static/css/main.af771717.chunk.css"
+  },
+  {
+    "revision": "6b7320b70aa50f0c523b",
+    "url": "./emotionale_unterstuetzung/static/js/2.c4b9f08d.chunk.js"
+  },
+  {
+    "revision": "723c7de9b804d224de1e",
+    "url": "./emotionale_unterstuetzung/static/js/3.65a8b092.chunk.js"
+  },
+  {
+    "revision": "0a41759d87c207fe0e93",
+    "url": "./emotionale_unterstuetzung/static/js/main.90655176.chunk.js"
+  },
+  {
+    "revision": "61a47174c99c7b114f49",
+    "url": "./emotionale_unterstuetzung/static/js/runtime~main.ac0f2b5c.js"
+  },
+  {
+    "revision": "065c894644d852cc081b3482dfa0f550",
+    "url": "./emotionale_unterstuetzung/static/media/picture1.065c8946.jpg"
+  }
+]);
---
+++ precache-manifest.48ffa9d1345a273d8914ef0f7eadebd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5e623adca0443e284932049c8de8d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b7320b70aa50f0c523b",
+    "url": "./static/css/2.016185da.chunk.css"
+  },
+  {
+    "revision": "0a41759d87c207fe0e93",
+    "url": "./static/css/main.af771717.chunk.css"
+  },
+  {
+    "revision": "6b7320b70aa50f0c523b",
+    "url": "./static/js/2.c4b9f08d.chunk.js"
+  },
+  {
+    "revision": "723c7de9b804d224de1e",
+    "url": "./static/js/3.65a8b092.chunk.js"
+  },
+  {
+    "revision": "0a41759d87c207fe0e93",
+    "url": "./static/js/main.90655176.chunk.js"
+  },
+  {
+    "revision": "27b2865ea30befcadea0",
+    "url": "./static/js/runtime~main.d0423071.js"
+  },
+  {
+    "revision": "065c894644d852cc081b3482dfa0f550",
+    "url": "./static/media/picture1.065c8946.jpg"
+  }
+]);
